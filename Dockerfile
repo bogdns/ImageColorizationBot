c76862938f52a787492b35e061c036a6f4891838
Bogdan Salimov <60847430+bogdns@users.noreply.github.com>
2024-09-21 23:36:01 +0300

    Added telegram bot

--- Dockerfile
+++ Dockerfile
@@ -10,11 +10,8 @@ RUN apt-get install wget -y
 
 COPY . /app
 
-ENV VIRTUAL_ENV=/venv
-ENV PATH="/venv/bin:$PATH"
-
-RUN /venv/bin/pip install --upgrade pip && \
-    /venv/bin/pip install -r requirements.txt
+RUN pip install --upgrade pip && \
+    pip install --no-cache-dir -r requirements.txt
 
 RUN ["chmod", "+x", "./docker-entrypoint.sh"]
 
---
+++ bot/__main__.py
@@ -0,0 +1,23 @@
+import asyncio
+import logging
+import os
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=os.getenv('TOKEN'))
+dp = Dispatcher()
+
+
+@dp.message(Command('start'))
+async def cmd_start(message: types.Message):
+    await message.answer('Hello!')
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
